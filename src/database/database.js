80e4fc00dfc20010f2f030321624af5240f57800
rgarza237 <roberto.garzah@udem.edu>
2022-05-14 17:57:39 -0500

    ultimaversion

--- src/database/database.js
+++ src/database/database.js
@@ -1,15 +1,20 @@
 //const { Sequelize } = require('sequelize');
 import { Sequelize } from 'sequelize'
 
- const sequelize = new Sequelize(
+const sequelize = new Sequelize(
     process.env.PG_DATABASE,
     process.env.PG_USER,
     process.env.PG_PASSWORD,
 {
- host: process.env.PG_HOST,
+dialect: 'postgres' ,  
+host: process.env.PG_HOST,
+port:5432,
  //host: 'http://3.218.67.164',
- dialect: 'postgres'
 })
+console.log(process.env.PG_HOST)
+
+//const sequelize = new Sequelize('postgres://roberto_garza:Enroute132:4000/roberto_hernandezdb')
+
 
 export default sequelize;
 
